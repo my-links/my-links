1110734394c302ed02f026df88284a54a84ac839
Sonny <sonnyasdev@gmail.com>
2023-02-08 16:49:49 +0100

    Update deps & add docker

--- config.ts
+++ config.ts
@@ -1,33 +1,3 @@
-// const NEXT_PUBLIC_PREFIX = 'NEXT_PUBLIC_';
-
-// const getEnvironmentVariable = (environmentVariable: string = ''): string => {
-//     if (!environmentVariable.startsWith(NEXT_PUBLIC_PREFIX)) {
-//         throw new Error(
-//             `Env var must start with "${NEXT_PUBLIC_PREFIX}": ${environmentVariable}`
-//         );
-//     }
-
-//     const envVarObject = Object
-//         .entries(process.env)
-//         .map(([key, value]) => ({ name: key, value }))
-//         .find((env) => {
-//             console.log(env.name === environmentVariable);
-//             return env.name === environmentVariable;
-//         });
-
-//     console.log('envVarObject', envVarObject, process.env.NEXT_PUBLIC_SITE_NAME)
-
-//     if (!envVarObject) {
-//         throw new Error(
-//             `Couldn't find environment variable: ${environmentVariable}`
-//         );
-//     } else {
-//         return envVarObject.value;
-//     }
-// };
-
 export const config = {
-    siteName: process.env.NEXT_PUBLIC_SITE_NAME // getEnvironmentVariable(NEXT_PUBLIC_PREFIX + 'SITE_NAME'),
+    siteName: "My Links",
 };
-
-console.log('config', config)
---
+++ docker/start.sh
@@ -0,0 +1,1 @@
+docker-compose --env-file ../.env -f ./docker-compose.yml up -d
