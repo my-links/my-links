a6f32e852cc7335d7244006d1e77f795b33fc233
Sonny <sonnyasdev@gmail.com>
2023-06-03 20:12:21 +0200

    feat: create functions to get user specific data
    Such as link, category or user related

--- src/lib/category/getUserCategory.ts
+++ src/lib/category/getUserCategory.ts
@@ -1,10 +1,11 @@
-import { User } from "@prisma/client";
+import { Category, User } from "@prisma/client";
 import prisma from "utils/prisma";
 
-export default async function getUserCategories(user: User) {
-  return await prisma.category.findMany({
+export default async function getUserCategory(user: User, id: Category["id"]) {
+  return await prisma.category.findFirst({
     where: {
       authorId: user?.id,
+      id,
     },
     include: {
       links: {
---
+++ src/lib/link/getLinkFromCategoryByName.ts
@@ -0,0 +1,19 @@
+import { Category, Link, User } from "@prisma/client";
+import prisma from "utils/prisma";
+
+export default async function getLinkFromCategoryByName(
+  user: User,
+  name: Link["name"],
+  categoryId: Category["id"]
+) {
+  return await prisma.link.findFirst({
+    where: {
+      authorId: user.id,
+      name,
+      categoryId,
+    },
+    include: {
+      category: true,
+    },
+  });
+}
