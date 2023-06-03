cfcc99fa6b4e0e9c6e0acfdaa8aa8db83190f078
Sonny <sonnyasdev@gmail.com>
2023-06-03 20:09:26 +0200

    feat: api handler

--- src/utils/session.ts
+++ src/utils/session.ts
@@ -3,11 +3,15 @@ import { getServerSession } from "next-auth/next";
 
 import { authOptions } from "pages/api/auth/[...nextauth]";
 
+export async function getSession(req: NextApiRequest, res: NextApiResponse) {
+  return await getServerSession(req, res, authOptions);
+}
+
 export async function getSessionOrThrow(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const session = await getServerSession(req, res, authOptions);
+  const session = await getSession(req, res);
 
   if (!session) {
     throw new Error("You must be connected");
