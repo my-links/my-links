f3e0b8afaee55685fafa1fd6378800ff4fef951a
Sonny <sonnyasdev@gmail.com>
2023-12-14 01:01:22 +0100

    fix: wrong table name in sql query

--- prisma/queries/set_default_next_id.sql
+++ prisma/queries/set_default_next_id.sql
@@ -1,10 +1,10 @@
 -- set default next id for each category based on LEAD(id) (LEAD -> Next)
 
-UPDATE Category AS c
+UPDATE category AS c
 JOIN (
     SELECT
         id,
         LEAD(id) OVER (PARTITION BY authorId ORDER BY id) AS nextCategoryId
-    FROM Category
+    FROM category
 ) AS n ON c.id = n.id
 SET c.nextId = n.nextCategoryId;
