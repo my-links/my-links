ac989db3d65bb5c86323891eeda7402b72213d44
Sonny <sonnyasdev@gmail.com>
2024-04-28 23:25:01 +0200

    fix: relations between tables

--- app/middleware/auth_middleware.ts
+++ app/middleware/auth_middleware.ts
@@ -1,6 +1,7 @@
+import PATHS from '#constants/paths';
+import type { Authenticators } from '@adonisjs/auth/types';
 import type { HttpContext } from '@adonisjs/core/http';
 import type { NextFn } from '@adonisjs/core/types/http';
-import type { Authenticators } from '@adonisjs/auth/types';
 
 /**
  * Auth middleware is used authenticate HTTP requests and deny
@@ -10,7 +11,7 @@ export default class AuthMiddleware {
   /**
    * The URL to redirect to, when authentication fails
    */
-  redirectTo = '/login';
+  redirectTo = PATHS.AUTH.LOGIN;
 
   async handle(
     ctx: HttpContext,
@@ -19,7 +20,9 @@ export default class AuthMiddleware {
       guards?: (keyof Authenticators)[];
     } = {}
   ) {
-    await ctx.auth.authenticateUsing(options.guards, { loginRoute: this.redirectTo });
+    await ctx.auth.authenticateUsing(options.guards, {
+      loginRoute: this.redirectTo,
+    });
     return next();
   }
 }
