d01bfaeec2de659e24e6682252d495f4e823be4e
Sonny <sonnyasdev@gmail.com>
2024-11-03 19:37:45 +0100

    feat: recreate favorite list/item

---
+++ inertia/mantine/components/dashboard/favorite/item/favorite_item.tsx
@@ -0,0 +1,25 @@
+import { Card, Group, Text } from '@mantine/core'; // Import de Mantine
+import { ExternalLinkStyled } from '~/components/common/external_link_styled';
+import LinkFavicon from '~/components/dashboard/link/link_favicon';
+import { LinkWithCollection } from '~/types/app';
+import styles from './favorite_item.module.css';
+
+export const FavoriteItem = ({
+	link: { name, url, collection },
+}: {
+	link: LinkWithCollection;
+}) => (
+	<Card className={styles.linkWrapper} radius="sm" withBorder>
+		<Group justify="center" gap="xs">
+			<LinkFavicon url={url} />
+			<ExternalLinkStyled href={url} style={{ flex: 1 }}>
+				<div className={styles.linkName}>
+					<Text lineClamp={1}>{name} </Text>
+				</div>
+				<Text c="gray" size="xs" lineClamp={1}>
+					{collection.name}
+				</Text>
+			</ExternalLinkStyled>
+		</Group>
+	</Card>
+);
