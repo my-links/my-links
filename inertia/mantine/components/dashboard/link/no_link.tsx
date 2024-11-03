40069905fa4e74a95b35e9b7a61adc0e24e64aa5
Sonny <sonnyasdev@gmail.com>
2024-11-03 19:55:34 +0100

    feat: recreate no link component

---
+++ inertia/mantine/components/dashboard/link/no_link.tsx
@@ -0,0 +1,37 @@
+import { Link } from '@inertiajs/react';
+import { route } from '@izzyjs/route/client';
+import { Anchor, Box, Text } from '@mantine/core';
+import { useTranslation } from 'react-i18next';
+import useActiveCollection from '~/hooks/use_active_collection';
+import { appendCollectionId } from '~/lib/navigation';
+import styles from './no_link.module.css';
+
+export function NoLink() {
+	const { t } = useTranslation('common');
+	const { activeCollection } = useActiveCollection();
+	return (
+		<Box className={styles.noCollection} p="xl">
+			<Text
+				className={styles.text}
+				dangerouslySetInnerHTML={{
+					__html: t(
+						'home:no-link',
+						{ name: activeCollection?.name ?? '' } as any,
+						{
+							interpolation: { escapeValue: false },
+						}
+					),
+				}}
+			/>
+			<Anchor
+				component={Link}
+				href={appendCollectionId(
+					route('link.create-form').path,
+					activeCollection?.id
+				)}
+			>
+				{t('link.create')}
+			</Anchor>
+		</Box>
+	);
+}
