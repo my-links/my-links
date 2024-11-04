8b24354c0e1df44b5da444217ce35ae5772634ce
Sonny <sonnyasdev@gmail.com>
2024-11-04 01:02:46 +0100

    feat: use link action dropdown for favorite links

--- inertia/mantine/components/dashboard/favorite/item/favorite_item.tsx
+++ inertia/mantine/components/dashboard/favorite/item/favorite_item.tsx
@@ -1,25 +1,23 @@
-import { Card, Group, Text } from '@mantine/core'; // Import de Mantine
+import { Card, Group, Text } from '@mantine/core';
 import { ExternalLinkStyled } from '~/components/common/external_link_styled';
+import LinkControls from '~/components/dashboard/link/link_controls';
 import LinkFavicon from '~/components/dashboard/link/link_favicon';
 import { LinkWithCollection } from '~/types/app';
 import styles from './favorite_item.module.css';
 
-export const FavoriteItem = ({
-	link: { name, url, collection },
-}: {
-	link: LinkWithCollection;
-}) => (
+export const FavoriteItem = ({ link }: { link: LinkWithCollection }) => (
 	<Card className={styles.linkWrapper} radius="sm" withBorder>
 		<Group justify="center" gap="xs">
-			<LinkFavicon size={32} url={url} />
-			<ExternalLinkStyled href={url} style={{ flex: 1 }}>
+			<LinkFavicon size={32} url={link.url} />
+			<ExternalLinkStyled href={link.url} style={{ flex: 1 }}>
 				<div className={styles.linkName}>
-					<Text lineClamp={1}>{name} </Text>
+					<Text lineClamp={1}>{link.name} </Text>
 				</div>
-				<Text c="gray" size="xs" lineClamp={1}>
-					{collection.name}
+				<Text c="gray" size="xs" mb={4} lineClamp={1}>
+					{link.collection.name}
 				</Text>
 			</ExternalLinkStyled>
+			<LinkControls link={link} showGoToCollection />
 		</Group>
 	</Card>
 );
