d68fcd9fc84c982e96ec65efb572a5bae3095f29
Sonny <sonnyasdev@gmail.com>
2024-11-09 23:48:14 +0100

    feat: persist user language

--- inertia/components/common/language_switcher.tsx
+++ inertia/components/common/language_switcher.tsx
@@ -1,5 +1,6 @@
 import { ActionIcon, Image } from '@mantine/core';
 import { useTranslation } from 'react-i18next';
+import { LS_LANG_KEY } from '~/constants';
 
 export function MantineLanguageSwitcher() {
 	const { i18n } = useTranslation();
@@ -8,7 +9,10 @@ export function MantineLanguageSwitcher() {
 		<ActionIcon
 			variant="light"
 			aria-label="Toggle color scheme"
-			onClick={() => i18n.changeLanguage(newLanguage)}
+			onClick={() => {
+				i18n.changeLanguage(newLanguage);
+				localStorage.setItem(LS_LANG_KEY, newLanguage);
+			}}
 			size="lg"
 		>
 			<Image src={`/icons/${newLanguage}.svg`} alt={newLanguage} w={18} />
