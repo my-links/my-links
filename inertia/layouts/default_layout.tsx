1b7c94db5e63bc9e4bc991c0884eb6b627917b93
Sonny <sonnyasdev@gmail.com>
2025-12-10 23:55:40 +0100

    fix: page transitioning target

--- inertia/layouts/default_layout.tsx
+++ inertia/layouts/default_layout.tsx
@@ -4,13 +4,12 @@ import { BaseLayout } from '~/layouts/base_layout';
 
 export const DefaultLayout = ({ children }: React.PropsWithChildren) => (
 	<BaseLayout>
-		<div
-			className="bg-gray-50 dark:bg-gray-900 h-screen overflow-hidden"
-			data-page-transition
-		>
+		<div className="bg-gray-50 dark:bg-gray-900 h-screen overflow-hidden">
 			<div className="h-full max-w-[1920px] mx-auto p-4 flex flex-col gap-6">
 				<Navbar />
-				<div className="flex-1 min-h-0 flex flex-col">{children}</div>
+				<div className="flex-1 min-h-0 flex flex-col" data-page-transition>
+					{children}
+				</div>
 				<Footer />
 			</div>
 		</div>
