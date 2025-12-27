9e7f21fb7d0f33537e4a1eb10cb5d5dbb56d7a61
Sonny <sonnyasdev@gmail.com>
2025-12-27 19:11:25 +0100

    refactor: remove unused user display preferences

--- app/lib/user/index.ts
+++
@@ -1,8 +0,0 @@
-import { getDisplayPreferences } from '#shared/lib/display_preferences';
-import type { DisplayPreferences } from '#shared/types/index';
-
-export function ensureDisplayPreferences(
-	value: Partial<DisplayPreferences> | DisplayPreferences | null | undefined
-): DisplayPreferences {
-	return getDisplayPreferences(value ?? undefined);
-}
