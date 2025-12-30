42aa4b65122a7ae4b0bb2d09e45081407e46d65f
Sonny <sonnyasdev@gmail.com>
2025-12-30 01:13:06 +0100

    refactor: remove unused get total links count method

--- app/controllers/api/links/delete_link_controller.ts
+++ app/controllers/api/links/delete_link_controller.ts
@@ -3,7 +3,6 @@ import { LinkService } from '#services/links/link_service';
 import { deleteLinkValidator } from '#validators/links/delete_link_validator';
 import { inject } from '@adonisjs/core';
 import { HttpContext } from '@adonisjs/core/http';
-import db from '@adonisjs/lucid/services/db';
 
 @inject()
 export default class DeleteLinkController {
@@ -19,10 +18,5 @@ export default class DeleteLinkController {
 		await this.linkService.deleteLink(params.id);
 
 		return this.collectionsService.redirectToCollectionId(link.collectionId);
-	}
-
-	private async getTotalLinksCount() {
-		const totalCount = await db.from('links').count('* as total');
-		return Number(totalCount[0].total);
 	}
 }
