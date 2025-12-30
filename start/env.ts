f476cb45442da70b44c9a7f29e2a91d6da506248
Sonny <sonnyasdev@gmail.com>
2025-12-30 01:09:51 +0100

    chore: remove no longer used pwa stuff

--- start/env.ts
+++ start/env.ts
@@ -1,14 +1,3 @@
-/*
-|--------------------------------------------------------------------------
-| Environment variables service
-|--------------------------------------------------------------------------
-|
-| The `Env.create` method creates an instance of the Env service. The
-| service validates the environment variables and also cast values
-| to JavaScript data types.
-|
-*/
-
 import { Env } from '@adonisjs/core/env';
 
 export default await Env.create(new URL('../', import.meta.url), {
--- start/kernel.ts
+++ start/kernel.ts
@@ -1,13 +1,3 @@
-/*
-|--------------------------------------------------------------------------
-| HTTP kernel file
-|--------------------------------------------------------------------------
-|
-| The HTTP kernel file is used to register the middleware with the server
-| or the router.
-|
-*/
-
 import router from '@adonisjs/core/services/router';
 import server from '@adonisjs/core/services/server';
 
