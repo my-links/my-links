af3df1ff3835dce11f1ebee79f60ad502ab12af9
Sonny <sonnyasdev@gmail.com>
2025-12-31 01:34:06 +0100

    fix: “should not already be working” caused by tuyau being recreated on every page change

--- inertia/layouts/base_layout.tsx
+++ inertia/layouts/base_layout.tsx
@@ -1,30 +1,22 @@
-import { api } from '#adonis/api';
 import { DEFAULT_LOCALE } from '#shared/consts/i18n';
 import { Locale } from '#shared/types/i18n';
 import { PageProps } from '@adonisjs/inertia/types';
 import { usePage } from '@inertiajs/react';
 import { i18n } from '@lingui/core';
 import { I18nProvider } from '@lingui/react';
-import { createTuyau } from '@tuyau/client';
 import { TuyauProvider } from '@tuyau/inertia/react';
 import { ReactNode, useEffect, useMemo } from 'react';
 import 'virtual:uno.css';
 import '~/css/app.css';
 import { usePageTransition } from '~/hooks/use_page_transition';
 import { dynamicActivate } from '~/i18n';
-
-const appUrl = import.meta.env.VITE_APP_URL;
+import { tuyauClient } from '~/lib/tuyau';
 
 export function BaseLayout({ children }: { children: ReactNode }) {
 	const { props } = usePage<PageProps & { locale: Locale }>();
 
 	usePageTransition({
 		querySelector: '[data-page-transition]',
-	});
-
-	const tuyauClient = createTuyau({
-		api,
-		baseUrl: appUrl,
 	});
 
 	const locale = useMemo(() => {
