75bbb07bef78a7474e421b521393479c062ecb41
Sonny <sonnyasdev@gmail.com>
2026-01-11 16:44:01 +0100

    refactor: replace deprecated vine.compile with vine.create

--- app/validators/user/token/delete_api_token.ts
+++ app/validators/user/token/delete_api_token.ts
@@ -1,6 +1,6 @@
 import vine from '@vinejs/vine';
 
-export const deleteApiTokenValidator = vine.compile(
+export const deleteApiTokenValidator = vine.create(
 	vine.object({
 		params: vine.object({
 			tokenId: vine.string().trim().minLength(1).maxLength(255),
