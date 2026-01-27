3e840fbebe0858b535cee510ef802247679f9a68
Sonny <sonnyasdev@gmail.com>
2026-01-27 15:50:20 +0100

    refactor: normalize link urls

--- app/validators/links/base_link_validator.ts
+++ app/validators/links/base_link_validator.ts
@@ -3,7 +3,7 @@ import vine from '@vinejs/vine';
 export const baseLinkValidator = vine.object({
 	name: vine.string().trim().minLength(1).maxLength(254),
 	description: vine.string().trim().maxLength(300).optional(),
-	url: vine.string().url({ require_tld: false }).trim(),
+	url: vine.string().normalizeUrl({ defaultProtocol: 'https' }).trim(),
 	favorite: vine.boolean(),
 	collectionId: vine.number(),
 });
